89a0746324a43eea380af8512eb47be05ec25deb
Donnellim <donjuanitoeduardoslim@iskolarngbayan.pup.edu.ph>
2025-07-21 16:22:24 +0800

    try

--- members/views.py
+++ members/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from django.template import loader
 from .models import Member
 
@@ -22,9 +22,10 @@ def main(request):
   template = loader.get_template('main.html')
   return HttpResponse(template.render())
 
+
 def testing(request):
   template = loader.get_template('template.html')
   context = {
-    'fruits': ['Apple', 'Banana', 'Cherry'],   
+    'emptytestobject': [],
   }
   return HttpResponse(template.render(context, request))
